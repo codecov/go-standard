18a9fd00b21aadf49c19b673e0e8f0903b9b875f
Joe Becher <71270647+drazisil-codecov@users.noreply.github.com>
2020-11-02 15:40:33 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package goStandard
 
+// I'm a comment!
+
 func UncoveredIf(a bool) bool {
 	if a {
 		return false
 	}
+	// I'm also a comment
 	return true
 }
+
 
 func FullyCovered() bool {
 	return true
 }
+/* Comment 3
+* space
+*/
 
 func Uncovered() bool {
 	return true
